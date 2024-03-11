15b8b965f31e9e1f574511905bb9d6461ca3d2c9
Ahmeddik <ahmeddik22@gmail.com>
2024-03-11 20:36:59 +0200

    update import

--- src/test/java/TestAddedItemToCart.java
+++ src/test/java/TestAddedItemToCart.java
@@ -1,6 +1,6 @@
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
