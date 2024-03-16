6d88dfb066adb5f24b31e1dbf3bc2c22ce956948
Ahmeddik <ahmeddik22@gmail.com>
2024-03-16 22:24:38 +0200

    add wait before assertion and assert with the item ID's insted of class name

--- src/test/java/TestAddedItemToCart.java
+++ src/test/java/TestAddedItemToCart.java
@@ -1,12 +1,16 @@
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import org.openqa.selenium.support.ui.*;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.time.Duration;
 
 public class TestAddedItemToCart {
 
     private WebDriver driver;
+    private WebDriverWait wait;
     String url = "https://www.saucedemo.com/";
     String userName = "standard_user";
     String passWord = "secret_sauce";
@@ -14,11 +18,13 @@ public class TestAddedItemToCart {
     By passWordInput = By.id("password");
     By logInButton = By.id("login-button");
 
+    String itemtName = "Sauce Labs Backpack";
     String xPath = "//div[@class=\"inventory_item_name \" and text()=\"%s\"] /parent::a/parent::div/following-sibling::div/button[@class=\"btn btn_primary btn_small btn_inventory \"]";
-    String xPathWithName = String.format(xPath,"Sauce Labs Backpack");
+    String xPathWithName = String.format(xPath,itemtName);
     By xPathOfAddToCartButton = By.xpath(xPathWithName);
     By cartPage = By.cssSelector("a span");
     By checkItem = By.xpath("//div[@class=\"inventory_item_name\"]");
+    By checkItemByID = By.id("item_4_title_link");
     String elementName;
     /*
     By inventoryItemName = By.cssSelector("div .inventory_item");
@@ -42,11 +48,21 @@ public class TestAddedItemToCart {
         driver.findElement(userNameInput).sendKeys(userName);
         driver.findElement(passWordInput).sendKeys(passWord);
         driver.findElement(logInButton).click();
-
+        /*
+        driver.findElement(xPathOfAddToCartButton).click();
+        xPathWithName = String.format(xPath,"Sauce Labs Bike Light");
+        xPathOfAddToCartButton = By.xpath(xPathWithName);
+        */
         driver.findElement(xPathOfAddToCartButton).click();
         driver.findElement(cartPage).click();
         elementName = driver.findElement(checkItem).getText();
-        Assert.assertEquals(elementName,"Sauce Labs Backpack");
+        //Assert.assertEquals(elementName,itemtName);
+        WebElement itemID = driver.findElement(checkItemByID);
+
+        wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(checkItem));
+        Assert.assertTrue(itemID.isDisplayed());
         driver.close();
     }
 }
